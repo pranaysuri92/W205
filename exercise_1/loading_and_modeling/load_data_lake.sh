accfa8097067dfa444ff73eb472cbca18b8b8879
pranaysuri92 <pranaysuri@pranays-mbp.attlocal.net>
2017-02-25 12:19:19 -0800

    exercise 1

---
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -0,0 +1,19 @@
+#Loading and modeling exercise 1
+
+#renamed files on local directory to elimnate spaces and capitalization
+#removed title lines in AWS instance using a script
+tail -n +2 hgi.csv  > geninfo.csv
+tail -n +2 md.csv  > codemapping.csv
+tail -n +2 tech.csv > proceduretec.csv
+tail -n +2 hvbp.csv > survey.csv
+tail -n +2 rdh.csv > procedurerd.csv
+
+#created a folder in hdfs called hospital_compare
+hdfs dfs -mkdir /user/w205/hospital_compare
+
+#loaded my files into the hdfs folder
+hdfs dfs -put geninfo.csv /user/w205/hospital_compare
+hdfs dfs -put codemapping.csv /user/w205/hospital_compare
+hdfs dfs -put proceduretec.csv /user/w205/hospital_compare
+hdfs dfs -put survey.csv /user/w205/hospital_compare
+hdfs dfs -put procedurerd.csv /user/w205/hospital_compare
