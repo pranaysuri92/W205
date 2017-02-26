2148bc1d56d66b4085fb68fa72c6324d47119e50
pranaysuri92 <pranaysuri@pranays-mbp.attlocal.net>
2017-02-25 21:40:26 -0800

    modified sql

--- exercise_1/loading_and_modeling/hive_base_ddl.sql
+++ exercise_1/loading_and_modeling/hive_base_ddl.sql
@@ -1,10 +1,14 @@
 DROP TABLE geninfo;
 CREATE EXTERNAL TABLE IF NOT EXISTS geninfo 
-(provider_id int, hospital_name string, 
+(provider_id string, hospital_name string, 
 address string, city string, state string, zip_code string, country_name string, phone_number string, 
 hospital_type string, hospital_ownership string, emergency_services string)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/geninfo';
 
@@ -12,11 +16,15 @@ SELECT COUNT(*) FROM geninfo;
 
 DROP TABLE codemapping;
 CREATE EXTERNAL TABLE IF NOT EXISTS codemapping
-(measure_name string, measure_id int,
+(measure_name string, measure_id string,
 measure_start_quarter string, measure_start_date string, 
 measure_end_quarter string, measure_end_date string)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/codemapping';
 
@@ -25,13 +33,17 @@ select count(*) from codemapping;
 
 DROP TABLE procedurerd;
 CREATE EXTERNAL TABLE IF NOT EXISTS procedurerd
-(provider_id int, hospital_name string,
+(provider_id string, hospital_name string,
 address string, city string, state string, zip_code string, country_name string, phone_number string,
-measure_name string, measure_id int, compare_to_national string, denominator int, 
+measure_name string, measure_id string, compare_to_national string, denominator string, 
 score string, lower_estimate string, higher_estimate string, footnote string, measure_start_date string, 
 measure_end_date string )
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/procedurerd';
 
@@ -39,11 +51,15 @@ select count(*) from procedurerd;
 
 DROP TABLE proceduretec;
 CREATE EXTERNAL TABLE IF NOT EXISTS proceduretec
-(provider_id int, hospital_name string,
+(provider_id string, hospital_name string,
 address string, city string, state string, zip_code string, country_name string, phone_number string,
-condition string, measure_id int, measure_name string, score string, sample string, footnote string, measure_start_date string, measure_end_date string)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+condition string, measure_id string, measure_name string, score string, sample string, footnote string, measure_start_date string, measure_end_date string)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/proceduretec';
 
@@ -52,14 +68,18 @@ select count(*) from proceduretec;
 
 DROP TABLE survey;
 CREATE EXTERNAL TABLE IF NOT EXISTS survey
-(provider_id int, hospital_name string,
+(provider_id string, hospital_name string,
 address string, city string, state string, zip_code string, country_name string,
 NAP string, NIP string, NDS string, DAP string, DIP string, DDS string, SAP string, SIP string,
 SDS string, PAP string, PIP string, PDS string, MMAP string, MMIP string, MMDS string, EAP string, EIP string,
 EDS string, DIAP string, DIIP string, DIDS string, OAP string, OIP string, ODS string, base_score string, 
 consistency_score string)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+ "escapeChar" = '\\'
+)
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/survey';
 
